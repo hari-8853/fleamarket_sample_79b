7803b77234d354558f1eac1c37e4cd2b3262d9cd
KSuga-tech <ksuga.5555@gmail.com>
2020-09-29 22:55:15 +0900

    Update 20200929130007_devise_create_users.rb
    DB設計に基づいたテーブルを構築

--- db/migrate/20200929130007_devise_create_users.rb
+++ db/migrate/20200929130007_devise_create_users.rb
@@ -4,9 +4,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,   null: false
+      t.string :email,      null: false, unique: true, index: true
+      t.string :password,   null: false
+      t.string :first_name, null: false
+      t.string :family_name, null: false
+      t.string :first_name_kana, null: false
+      t.string :family_name_kana, null: false
 
+      t.string :city, null: false
+      t.string :house_number, null:false
+      t.string :building_name
+      t.string :phone_number, unique: true
+      t.date :birth_date, null:false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
