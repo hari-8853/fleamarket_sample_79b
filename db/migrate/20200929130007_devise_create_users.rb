b66d61b36f753b7bc188b68ae077c7cd4a8bb8f9
KSuga-tech <ksuga.5555@gmail.com>
2020-09-30 06:26:13 +0900

    usersテーブルの作成
    ターミナル上で、
    rails db:migrateを行った。

--- db/migrate/20200929130007_devise_create_users.rb
+++ db/migrate/20200929130007_devise_create_users.rb
@@ -4,19 +4,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,   null: false
-      t.string :email,      null: false, unique: true, index: true
-      t.string :password,   null: false
-      t.string :first_name, null: false
-      t.string :family_name, null: false
-      t.string :first_name_kana, null: false
+      t.string :nickname,         null: false
+      t.string :email,            null: false, unique: true, index: true
+      t.string :password,         null: false
+      t.string :first_name,       null: false
+      t.string :family_name,      null: false
+      t.string :first_name_kana,  null: false
       t.string :family_name_kana, null: false
 
-      t.string :city, null: false
-      t.string :house_number, null:false
+      t.string :city,             null: false
+      t.string :house_number,     null: false
       t.string :building_name
-      t.string :phone_number, unique: true
-      t.date :birth_date, null:false
+      t.string :phone_number,                  unique: true
+      t.date :birth_date,         null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -46,8 +46,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :email,                unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
