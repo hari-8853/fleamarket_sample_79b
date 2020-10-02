14426b52a2d50a1464bc6e90a45ff7fd8fd755a6
KSuga-tech <ksuga.5555@gmail.com>
2020-10-02 22:02:15 +0900

    ヘッダーの仮置き
    ヘッダーを仮で作り、ログイン時のページと新規登録のページへのリンクをテストした。

--- db/migrate/20200929130007_devise_create_users.rb
+++ db/migrate/20200929130007_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :family_name,      null: false
       t.string :first_name_kana,  null: false
       t.string :family_name_kana, null: false
-
+      t.integer :prefecturecode,  null: false 
       t.string :city,             null: false
       t.string :house_number,     null: false
       t.string :building_name
