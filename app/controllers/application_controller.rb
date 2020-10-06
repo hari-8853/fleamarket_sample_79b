b2a7215641befb90cc62c1e759b86950b94dcfba
KSuga-tech <ksuga.5555@gmail.com>
2020-10-07 06:22:12 +0900

    Revert "sippai2"
    This reverts commit 8dd01ebe0fd960122cec7a13dcdf220834d5e709.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -13,9 +12,5 @@ class ApplicationController < ActionController::Base
 
   def production?
     Rails.env.production?
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_141005) do
+ActiveRecord::Schema.define(version: 2020_09_29_130007) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
