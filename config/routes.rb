55dbc24c0cb35a6fc4eba6458f22a93bd560cb73
Masayo KIkuchi <macha580@yahoo.co.jp>
2020-10-11 17:12:16 +0900

    Revert "Merge remote-tracking branch 'origin/newitem-page' into トップページ"
    This reverts commit 7b97b3b6e11b0bfc04851b5f05af2d770a1e9dd3, reversing
    changes made to fb2b021cd78fa086aaf670123b39e37e56067c2e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # by Kikuchi
   get 'home/index'
   root 'home#index'
-  # by Nakazima
-  root 'items#index'
-  resources :items, only: :new
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
