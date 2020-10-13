54f547b3973950f45c65b162cdf1b648051eb8b3
yuta-kamizuka <cybor_ggg@icloud.com>
2020-10-13 14:31:29 +0900

    複数枚の画像投稿実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,26 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.images.new
   end
+
+  
+  def create
+    @item = item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, images_attributes: [:src])
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items, only: :new
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items, only: [:new, :create,] do
+  end
 end
