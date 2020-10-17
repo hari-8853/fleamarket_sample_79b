465280af835e131564344a36ae36f9b0630bd17f
Masayo KIkuchi <macha580@yahoo.co.jp>
2020-10-17 14:23:10 +0900

    update itempage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :home, only: :index
+  resources :items, only: :new
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
