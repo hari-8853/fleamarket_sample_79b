77f24859ff0a652355e2dc1856cb35b1f5ad6083
hari-8853 <68177185+hari-8853@users.noreply.github.com>
2020-10-17 23:04:54 +0900

    Update routes.rb
    logout cragit profiele追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :home, only: :index
   resources :items, only: :new
-  resources :users, only: [:show, :edit]
+  resources :users, except: [:edit] do
+    member do
+      get 'edit'
+      get 'logout'
+      get 'credit_register'
+      get 'edit_profiele'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
