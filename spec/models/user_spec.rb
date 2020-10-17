e4b37af1a07a72045ad7c7c9b5eb2bdfb4c6fbd9
KSuga-tech <ksuga.5555@gmail.com>
2020-10-17 17:17:24 +0900

    テストコードの記述

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "is invalid without a nickname" do
+      user = User.new(
+        nickname: "",
+        email: "kkk@gmail.com",
+        password: "1111111",
+        # first_name: "a",
+        # family_name: "b",
+        # first_name_kana: "c",
+        # family_name_kana: "d",
+        # birth_date: "1995-10-12",
+        # post_code: "3270835",
+        # prefecture_code: 9,
+        # city: "f",
+        # house_number: "g",
+        # building_name: "g",
+        # phone_number: "08000000000"
+      )
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+  end
+end
