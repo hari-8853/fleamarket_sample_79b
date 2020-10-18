584144e6650293dd77b97d4386420a9116bbfc4c
KSuga-tech <ksuga.5555@gmail.com>
2020-10-18 15:44:01 +0900

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,15 +5,3 @@
 //= require jquery_ujs
 //= require jquery.jpostal
 //= require_tree .
-
-$(function() {
-  return $('#user_postcode').jpostal({
-    postcode: ['#user_postcode'],
-    address: {
-      '#user_prefecture_code': '%3',
-      '#user_city': '%4',
-      '#user_house_number': '%5%6%7',
-    },
-
-  });
-});
