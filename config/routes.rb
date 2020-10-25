489daa82b5953fbfcbebb6c9db7563328bc606bc
KSuga-tech <ksuga.5555@gmail.com>
2020-10-25 22:15:52 +0900

    購入確認画面のhaml作成
    purchaseコントローラーを作成し、
    purchase/new.html.hamlで表示できるようにしている。
    既存のコントローラー下に置くときには、このコントローラーは破棄する。

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources 'users', only: :show
   resources :home, only: :index
   resources :items, only: :new
+  resources :purchases, only: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
