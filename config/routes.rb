446cb8df0ef8d0f433eb10934318912729591cb8
yuta-kamizuka <cybor_ggg@icloud.com>
2020-10-27 21:46:31 +0900

    商品詳細のhaml作成
    ルーティング、コントローラにshow追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :home, only: :index
-  resources :items, only: :new
+  resources :items, only: [:new, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
