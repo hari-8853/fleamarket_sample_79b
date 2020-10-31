e2ae1004b339d6aeaa5bc062979874a1dab25458
hari-8853 <68177185+hari-8853@users.noreply.github.com>
2020-10-31 15:55:30 +0900

    credit_cards_controller.rb pay#3
    カードテーブルのデータ作成

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -11,6 +11,25 @@ class CreditCardsController < ApplicationController
     if params['payjp_token'].blank?
       redirect_to action: "new"
       
+    else
+      customer = Payjp::Customer.create(
+        description: '登録テスト',
+        
+        card: params['payjp_token'],
+        metadata: {user_id: current_user.id}
+      )
+      @card = CreditCard.new(
+        user_id: current_user.id,
+        customer_id: customer.id,
+        card_id: customer.default_card
+      )
+      
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
+      end
+    end
   end
 
   def show
