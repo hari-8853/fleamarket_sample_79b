04b50c7dfcf8ab4f564a118c4f6c0318f04b771e
hari-8853 <68177185+hari-8853@users.noreply.github.com>
2020-10-31 16:07:01 +0900

    ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
       get 'edit_profiele'
     end
   end
-  resources :credit_cards, only: [:index, :new, :show, :create, :destroy]
+  resources :credit_cards, only: [:new, :show,] do
+    collection do
+      post 'show', to: 'credit_cards#show'
+      post 'pay', to: 'credit_cards#pay'
+      post 'delete', to: 'credit_cards#delete'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
