5fa6ca59734f59d6457ebe9b64afe798589f6a53
hari-8853 <68177185+hari-8853@users.noreply.github.com>
2020-10-31 14:56:34 +0900

    Update credit_cards_controller.rb#2
    new
    カードがあればshowページへ

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,6 +2,9 @@ class CreditCardsController < ApplicationController
   require 'payjp'
 
   def new
+    card = CreditCard.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
+  end
 
   end
 
