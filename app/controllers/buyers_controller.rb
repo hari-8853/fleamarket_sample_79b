fa1a794440ea49af6bc4ae2e3d941973232f61e5
hari-8853 <68177185+hari-8853@users.noreply.github.com>
2020-11-03 12:28:07 +0900

    buyers_controller_2
    メソッドの設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,8 +1,19 @@
 class BuyersController < ApplicationController
   require 'payjp'
+  before_action :set_card, :set_item
   def index
   end
 
   def done
+  end
+
+  private
+
+  def set_card
+    @card = CreditCard.find_by(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
