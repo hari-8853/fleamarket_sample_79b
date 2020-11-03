fcee8d98b7bc574d39b785391144d08d65435558
hari-8853 <68177185+hari-8853@users.noreply.github.com>
2020-11-03 12:24:37 +0900

    購入機能のルーテイング

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   resources :home, only: :index
-  resources :buyers, only: :index
-  get 'done', to: 'buyers#done' 
-  resources :items, only: [:index, :new, :create, :update]
+  resources :items, only: [:index, :new, :create, :update] do
+    resources :buyers, only: [:index] do
+      collection do
+        get 'done', to: 'buyers#done'
+        post 'pay', to: 'buyers#pay'
+      end
+    end
+  end
   resources :users, except: [:edit] do
     member do
       get 'edit'
