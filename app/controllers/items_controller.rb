50a46298cfa2ae9736fd4f6e2737be90003d927e
Masayo KIkuchi <macha580@yahoo.co.jp>
2020-11-23 13:09:25 +0900

    edit/category追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  # before_action :set_category, only: [:new, :create, :edit, :update]
+  # before_action :set_item, only: [:show, :edit, :update]
  
   def index
     @items = Item.includes(:image).order('created_at DESC')
@@ -44,6 +46,15 @@ class ItemsController < ApplicationController
     # binding.pry
     @item = Item.find(params[:id])
     @item.images.build
+
+    def get_category_children
+      @category_children = Category.find(params[:parent_id]).children
+    end
+    
+    # 子カテゴリーが選択された後に動くアクション
+    def get_category_grandchildren
+      @category_grandchildren = Category.find(params[:child_id]).children
+    end
   end
 
   def update
@@ -79,6 +90,10 @@ class ItemsController < ApplicationController
   #   @category_parent_array = []
   #     Category.where(ancestry: nil).each do |parent|
   #       @category_parent_array << parent
-      # end
+  #     end
+  # end
+
+  # def set_item
+  #   @item = Item.find(params[:id])
   # end
 end
