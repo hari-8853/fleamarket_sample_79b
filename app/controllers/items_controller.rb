b4446aa3626e632ab037edaf86eaa3c9840a052e
Masayo KIkuchi <macha580@yahoo.co.jp>
2020-11-23 13:14:11 +0900

    削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,15 +46,6 @@ class ItemsController < ApplicationController
     # binding.pry
     @item = Item.find(params[:id])
     @item.images.build
-
-    def get_category_children
-      @category_children = Category.find(params[:parent_id]).children
-    end
-    
-    # 子カテゴリーが選択された後に動くアクション
-    def get_category_grandchildren
-      @category_grandchildren = Category.find(params[:child_id]).children
-    end
   end
 
   def update
