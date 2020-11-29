2570af774ddee3fb968104dd9d876d1ec7e2e9ec
KSuga-tech <ksuga.5555@gmail.com>
2020-11-29 22:34:34 +0900

    javascriptの修正
    リロードなしで郵便番号入力から住所が自動で入力される

--- app/assets/javascripts/address_autofill.js
+++ app/assets/javascripts/address_autofill.js
@@ -1,12 +1,13 @@
-$(function() {
-  return $('#user_post_code').jpostal({
-    postcode: ['#user_post_code'],
-    address: {
-      '#user_prefecture_code': '%3',
-      '#user_city': '%4',
-      '#user_house_number': '%5%6%7',
-    },
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    return $('#user_post_code').jpostal({
+      postcode: ['#user_post_code'],
+      address: {
+        '#user_prefecture_code': '%3',
+        '#user_city': '%4',
+        '#user_house_number': '%5%6%7',
+      },
 
+    });
   });
 });
-console.log('test');
